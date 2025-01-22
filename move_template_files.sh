be4befaef295b965a11a6c9162f5dbc3e259cdc1
jpizarro <jose.pizarro-blanco@bam.de>
2025-01-22 16:31:37 +0100

    Initial creation of parser

---
+++ move_template_files.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+if ! command -v rsync >/dev/null 2>&1; then
+  echo "rsync required, but not installed!"
+  exit 1
+else
+  rsync -avh nomad-parser-fireworks/ .
+  rm -rfv nomad-parser-fireworks
+fi
